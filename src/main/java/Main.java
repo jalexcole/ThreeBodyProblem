b9847d96fa19e26efa5349993d971d0548bd9995
alex <jalexcole@gmail.com>
2020-10-26 23:29:37 -0500

    Cleaned up Main File

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class Main {
-    ArrayList<Body> bodies = new ArrayList<>();
-    final double gravitationConstant = 6.6743015e-11;
 
     public static void main(String[] args) {
         BodySystem system = new BodySystem(20000);
