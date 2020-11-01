42e00739331cb64e4c958fb3cf414f15c556b6da
alex <jalexcole@gmail.com>
2020-11-01 13:38:51 -0600

    Body now has a Builder for setting parameters.

--- src/main/java/Body.java
+++ src/main/java/Body.java
@@ -7,23 +7,54 @@ class Body {
      * <p>
      * Inputs should only be for updates and returning position.
      */
-    int dimensionality = 3;
-    double[] position = new double[dimensionality];
-    double[] velocity = new double[dimensionality];
-    double[] acceleration = new double[dimensionality];
-    double mass;
+    private int dimensionality = 3;
+    private double[] position = new double[dimensionality];
 
-
+    // Set as public due to outside use
+    protected double[] velocity = new double[dimensionality];
+    protected double[] acceleration = new double[dimensionality];
+    protected double mass;
 
     private double modifier = 1;
     private boolean attracting = true;
     private double stepSize = 1;
 
-    Random random = new Random();
+    private Random random = new Random();
 
+    static class Builder {
+        private Random random = new Random();
+        private int dimensionality;
+        private double modifier;
+        private double stepSize;
 
+        public void setRandom(Random random) {
+            this.random = random;
+        }
 
+        public void setDimensionality(int dimensionality) {
+            this.dimensionality = dimensionality;
+        }
 
+        public void setModifier(double modifier) {
+            this.modifier = modifier;
+        }
+
+        public void setStepSize(double stepSize) {
+            this.stepSize = stepSize;
+        }
+
+        public Body build(){
+            return new Body(this);
+        }
+    }
+
+    public Body(Builder builder){
+        this.random = builder.random;
+        this.dimensionality = builder.dimensionality;
+        this.modifier = builder.modifier;
+        this.stepSize =builder.stepSize;
+        init();
+    }
 
     public Body() {
         init();
@@ -36,7 +67,7 @@ class Body {
 
 
     public void init() {
-        for (int i = 0; i < position.length; i++) {
+        for (int i = 0; i < dimensionality; i++) {
             position[i] = random.nextDouble() * modifier;
             velocity[i] = 0;
             acceleration[i] = 0;
@@ -94,18 +125,26 @@ class Body {
         return mass;
     }
 
+    public double[] getVelocity() {
+        return velocity;
+    }
+
+    public double[] getAcceleration() {
+        return acceleration;
+    }
+
     public void setDimensionality(int dimensionality) {
         this.dimensionality = dimensionality;
     }
 
     public double[] getMomentum() {
-
-        return null;
+        return Numeric.mulVector(velocity, mass);
     }
 
     public void setStepSize(double stepSize) {
         this.stepSize = stepSize;
     }
+
     public void setModifier(double modifier) {
         this.modifier = modifier;
     }
