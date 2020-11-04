2deca4b4a2e013a8acecc23b6a93e5d6f3dd05cd
alex <jalexcole@gmail.com>
2020-11-04 13:12:31 -0600

    Build a simulation class that will govern the NBody simulation application along with database management.

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,23 @@
-
+import java.sql.*;
 
 public class Simulation {
+    /**
+     * Simulation is the governing class of the N-Body Simulation.
+     * It is to create the system of objects of however many is needed and load
+     * into a single page database file.
+     *
+     * The Simulation class will create workers that operate upon the NBody.db file
+     * and then load results back into the .db file.
+     *
+     * The Simulation class will also act as a state machine to govern frame times.*/
+
+    public Simulation(){
+
+    }
+
+    public Simulation(String[] args){
+
+    }
+
 
 }
